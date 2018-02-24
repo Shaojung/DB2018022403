6a618bf5ef9559fd4e93b6c8c329fe565fad2c94
Shaojung <shaojung@gmail.com>
2018-02-24 14:01:45 +0800

    Thread

--- app/src/main/java/com/example/student/db2018022403/MainActivity.java
+++ app/src/main/java/com/example/student/db2018022403/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
+    int c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,16 +17,29 @@ public class MainActivity extends AppCompatActivity {
     }
     public void click1(View v)
     {
-        for (int i=5;i>=0;i--)
-        {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                for (int i=5;i>=0;i--)
+                {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    c = i;
+                    Log.d("LOG", "倒數:" + i);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv.setText(String.valueOf(c));
+                        }
+                    });
 
-            Log.d("LOG", "倒數:" + i);
-            tv.setText(String.valueOf(i));
-        }
+                }
+            }
+        }.start();
+
     }
 }
