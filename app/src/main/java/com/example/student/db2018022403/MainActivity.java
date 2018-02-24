765ffda6d5207dd0e2ee81b690f661fd957201a9
Shaojung <shaojung@gmail.com>
2018-02-24 13:54:15 +0800

    Thread

--- app/src/main/java/com/example/student/db2018022403/MainActivity.java
+++ app/src/main/java/com/example/student/db2018022403/MainActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
     }
     public void click1(View v)
     {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             Log.d("LOG", "倒數:" + i);
+            tv.setText(String.valueOf(i));
         }
     }
 }
